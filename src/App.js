ad609266edd9b8c986ecff08e3ffe28eca56601f
Sirarpy <sipadavtyan@gmail.com>
2021-02-17 18:22:43 +0400

    fetch git API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-
+import {Header} from "./Header/Header";
 
 function App() {
   return (
-  <React.Fragment>
-    <Header />
-  </React.Fragment>
+    <div className="App">
+        <Header />
+    </div>
   );
 }
 
